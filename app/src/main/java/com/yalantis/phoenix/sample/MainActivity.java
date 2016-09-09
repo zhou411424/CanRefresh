d6651ce49dca216208a573fbf21c933c06eb3817
zhoulc <zhouliancheng@xingyun.cn>
2016-09-09 11:16:58 +0800

    Initial commit

---
+++ app/src/main/java/com/yalantis/phoenix/sample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.yalantis.phoenix.sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yalantis.phoenix.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
